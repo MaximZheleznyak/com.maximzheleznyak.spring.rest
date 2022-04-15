d1bb516d7fa32b093bd7bfc73369e82a9cde372e
macbook <core.nod@yandex.ru>
2022-04-15 11:42:46 +0300

    Initial commit

---
+++ src/main/java/com/maximzheleznyak/spring/rest/controller/MyRestController.java
@@ -0,0 +1,68 @@
+package com.maximzheleznyak.spring.rest.controller;
+
+
+import com.maximzheleznyak.spring.rest.entity.Employee;
+import com.maximzheleznyak.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.maximzheleznyak.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.maximzheleznyak.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEployees(){
+        List<Employee> allEmployees= employeeService.getAllEmployees();
+        return allEmployees;
+    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id){
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null){
+
+            throw new NoSuchEmployeeException("There is no Employee with ID " + id + " in Database" );
+
+
+
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+    employeeService.saveEmployee(employee);
+    return employee;
+
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+        public String deleteEmployee (@PathVariable int id){
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if(employee==null){
+            throw new NoSuchEmployeeException("There is no employee with id =" + id);
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id "+id+" was deleted";
+    }
+}
